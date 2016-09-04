41a029c81df0f3ccb2bddacdd3d35cffa2bece63
Sneha Jain <snehajain@Snehas-MacBook-Pro.local>
2016-09-04 10:19:25 +0530

    initial commit

---
+++ components/Layout/Header.js
@@ -0,0 +1,30 @@
+/**
+ * React App SDK (https://github.com/kriasoft/react-app)
+ *
+ * Copyright © 2015-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import Navigation from './Navigation';
+import Link from '../Link';
+import s from './Header.css';
+
+function Header() {
+  return (
+    <header className={s.header}>
+      <div className={s.container}>
+        <Link className={s.title} to="/">
+          <img src="/logo.png" alt="Logo" />
+          <span>Top <strong>GIT</strong></span>
+        </Link>
+        <span className={s.subtitle}> - Finding the best github projects!!</span>
+        <Navigation />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
